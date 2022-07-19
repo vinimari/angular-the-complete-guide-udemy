b192c8204f2f936e532a042dbc787f28666bda3b
vinimari <viniciusmarinho44@hotmail.com>
2022-07-19 14:15:37 -0300

    feat: bindable properties added with @Input

--- directives-start/src/app/basic-highlight/better-highlight.directive.ts
+++ directives-start/src/app/basic-highlight/better-highlight.directive.ts
@@ -1,23 +1,26 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defautColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
   @HostBinding('style.backgroundColor') backgroundColor: string = 'trasparent'; 
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    this.backgroundColor = this.defautColor; 
   }
 
   @HostListener('mouseenter') mouseover(eventData: Event) {
   // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
-    this.backgroundColor = 'blue'; 
+    this.backgroundColor = this.highlightColor; 
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
   //  this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent'); 
-    this.backgroundColor ='transparent';
+    this.backgroundColor = this.defautColor;
   }
 }
