75e1de8d6845b18cd83f1fea427cbc10a34c4c10
vinimari <viniciusmarinho44@hotmail.com>
2022-07-22 18:38:41 -0300

    feat: services project added, Recipes Project dele

--- prj-basics-final/src/app/shared/dropdown.directive.ts
+++ prj-basics-final/src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
@@ -7,10 +7,11 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false; 
 
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen; 
+  @HostListener('document:click', ['$event']) toggleClpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false
+    
   }
 
-  constructor() { }
+  constructor(private elRef: ElementRef) { }
 
 }
