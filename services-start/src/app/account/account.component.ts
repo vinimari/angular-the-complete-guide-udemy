5e4a90c4c18af5440dbb02353364b86afffdb404
vinimari <viniciusmarinho44@hotmail.com>
2022-07-22 23:40:11 -0300

    feat: service accounts Service implemented

--- services-start/src/app/account/account.component.ts
+++ services-start/src/app/account/account.component.ts
@@ -1,21 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountsService } from '../accounts.service';
 import { LogginService } from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers: [LogginService]
+  providers: [LogginService, AccountsService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor( private logginService: LogginService) {}
+  constructor( private logginService: LogginService, private accountsService: AccountsService) {}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountsService.updateStatus(this.id, status); 
     this.logginService.logStatusChange(status); 
   }
 }
--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,23 +1,20 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountsService } from '../accounts.service';
 import { LogginService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LogginService]
+  providers: [LogginService, AccountsService]
 
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor(private logginService: LogginService) {}
+  constructor(private logginService: LogginService, private accountsService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountsService.addAccount(accountName, accountStatus); 
     this.logginService.logStatusChange(accountStatus);
   }
 }
