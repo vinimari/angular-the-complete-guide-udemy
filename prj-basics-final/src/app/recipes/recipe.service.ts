ca710a06157ebdddf6fef73791dbd4c52c2b16cc
vinimari <viniciusmarinho44@hotmail.com>
2022-08-03 21:51:46 -0300

    feat: recipe service created

---
+++ prj-basics-final/src/app/recipes/recipe.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RecipeService {
+
+  constructor() { }
+}
