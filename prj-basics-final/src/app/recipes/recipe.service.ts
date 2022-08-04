95d28e4e4e7e9292adca174d43b9d46b5c8bd173
vinimari <viniciusmarinho44@hotmail.com>
2022-08-03 22:14:24 -0300

    feat: recipe service implemented

--- prj-basics-final/src/app/recipes/recipe.service.ts
+++ prj-basics-final/src/app/recipes/recipe.service.ts
@@ -8,4 +8,13 @@ import { Recipe } from './recipe.model';
 export class RecipeService {
 
   constructor() { }
+
+    private recipes: Recipe[] = [
+    new Recipe('Another', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+  ];
+
+  getRecipes() {
+    return this.recipes.slice(); 
+  }
 }
